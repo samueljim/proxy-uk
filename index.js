35ed5bd52a183382376baae5b40904c8a7315915
sam <thesamueljim@gmail.com>
2019-11-05 08:25:00 +1000

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors')
+const request = require('request').defaults({ encoding: null });
+
+const app = express();
+app.use(cors());
+
+const asyncMiddleware = fnc =>
+    (req, res, next) => {
+        Promise.resolve(fnc(req, res, next))
+            .catch(next);
+    };
+
+app.get('/raw/:url', asyncMiddleware(async (req, res, next) => {
+    (req.params.url) ? request(req.params.url).pipe(res) : next('No url');
+}));
+
+app.get('*', asyncMiddleware(async (req, res, next) => {
+    (req.query.url) ? request(req.query.url).pipe(res) : res.send('Bad url');
+}));
+
+// if there's an error in routing then this will happen
+app.use(function (err, req, res, next) {
+    res.send({'error': err});
+    // res.end();
+});
+
+module.exports = app
